fbf6eb192f02e7d07f69f91a10a3a63adcc34981
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2016-06-05 16:33:46 -0400

    Add files via upload

---
+++ Methods1_9_R2/Methods/Methods1_9_R2.java
@@ -0,0 +1,29 @@
+package Methods;
+
+import org.bukkit.craftbukkit.v1_9_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import net.minecraft.server.v1_9_R2.NBTTagCompound;
+import net.minecraft.server.v1_9_R2.NBTTagList;
+
+public class Methods1_9_R2 implements MethodsClass {
+
+	@Override
+	public ItemStack addGlow(ItemStack paramItemStack) {
+		
+		net.minecraft.server.v1_9_R2.ItemStack localItemStack = CraftItemStack.asNMSCopy(paramItemStack);
+	    NBTTagCompound localNBTTagCompound = null;
+	    if (!localItemStack.hasTag()) {
+	      localNBTTagCompound = new NBTTagCompound();
+	      localItemStack.setTag(localNBTTagCompound);
+	    }
+	    if (localNBTTagCompound == null) localNBTTagCompound = localItemStack.getTag();
+
+	    NBTTagList localNBTTagList = new NBTTagList();
+	    localNBTTagCompound.set("ench", localNBTTagList);
+	    localItemStack.setTag(localNBTTagCompound);
+	    return CraftItemStack.asCraftMirror(localItemStack);
+	  }
+
+}
+
