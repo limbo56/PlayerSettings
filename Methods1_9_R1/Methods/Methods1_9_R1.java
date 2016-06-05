0bf021891ad0aac184944e607ba9b00a93b56c76
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2016-06-05 17:03:08 -0400

    Add files via upload

--- Methods1_9_R1/Methods/Methods1_9_R1.java
+++ Methods1_9_R1/Methods/Methods1_9_R1.java
@@ -6,23 +6,30 @@ import org.bukkit.inventory.ItemStack;
 import net.minecraft.server.v1_9_R1.NBTTagCompound;
 import net.minecraft.server.v1_9_R1.NBTTagList;
 
+/**
+ * 
+ * @author lim_bo56
+ *
+ */
+
 public class Methods1_9_R1 implements MethodsClass {
 
 	@Override
 	public ItemStack addGlow(ItemStack paramItemStack) {
-		
-		net.minecraft.server.v1_9_R1.ItemStack localItemStack = CraftItemStack.asNMSCopy(paramItemStack);
-	    NBTTagCompound localNBTTagCompound = null;
-	    if (!localItemStack.hasTag()) {
-	      localNBTTagCompound = new NBTTagCompound();
-	      localItemStack.setTag(localNBTTagCompound);
-	    }
-	    if (localNBTTagCompound == null) localNBTTagCompound = localItemStack.getTag();
 
-	    NBTTagList localNBTTagList = new NBTTagList();
-	    localNBTTagCompound.set("ench", localNBTTagList);
-	    localItemStack.setTag(localNBTTagCompound);
-	    return CraftItemStack.asCraftMirror(localItemStack);
-	  }
+		net.minecraft.server.v1_9_R1.ItemStack localItemStack = CraftItemStack.asNMSCopy(paramItemStack);
+		NBTTagCompound localNBTTagCompound = null;
+		if (!localItemStack.hasTag()) {
+			localNBTTagCompound = new NBTTagCompound();
+			localItemStack.setTag(localNBTTagCompound);
+		}
+		if (localNBTTagCompound == null)
+			localNBTTagCompound = localItemStack.getTag();
+
+		NBTTagList localNBTTagList = new NBTTagList();
+		localNBTTagCompound.set("ench", localNBTTagList);
+		localItemStack.setTag(localNBTTagCompound);
+		return CraftItemStack.asCraftMirror(localItemStack);
+	}
 
 }
--- Methods1_9_R2/Methods/Methods1_9_R2.java
+++ Methods1_9_R2/Methods/Methods1_9_R2.java
@@ -6,24 +6,30 @@ import org.bukkit.inventory.ItemStack;
 import net.minecraft.server.v1_9_R2.NBTTagCompound;
 import net.minecraft.server.v1_9_R2.NBTTagList;
 
+/**
+ * 
+ * @author lim_bo56
+ *
+ */
+
 public class Methods1_9_R2 implements MethodsClass {
 
 	@Override
 	public ItemStack addGlow(ItemStack paramItemStack) {
-		
-		net.minecraft.server.v1_9_R2.ItemStack localItemStack = CraftItemStack.asNMSCopy(paramItemStack);
-	    NBTTagCompound localNBTTagCompound = null;
-	    if (!localItemStack.hasTag()) {
-	      localNBTTagCompound = new NBTTagCompound();
-	      localItemStack.setTag(localNBTTagCompound);
-	    }
-	    if (localNBTTagCompound == null) localNBTTagCompound = localItemStack.getTag();
 
-	    NBTTagList localNBTTagList = new NBTTagList();
-	    localNBTTagCompound.set("ench", localNBTTagList);
-	    localItemStack.setTag(localNBTTagCompound);
-	    return CraftItemStack.asCraftMirror(localItemStack);
-	  }
+		net.minecraft.server.v1_9_R2.ItemStack localItemStack = CraftItemStack.asNMSCopy(paramItemStack);
+		NBTTagCompound localNBTTagCompound = null;
+		if (!localItemStack.hasTag()) {
+			localNBTTagCompound = new NBTTagCompound();
+			localItemStack.setTag(localNBTTagCompound);
+		}
+		if (localNBTTagCompound == null)
+			localNBTTagCompound = localItemStack.getTag();
+
+		NBTTagList localNBTTagList = new NBTTagList();
+		localNBTTagCompound.set("ench", localNBTTagList);
+		localItemStack.setTag(localNBTTagCompound);
+		return CraftItemStack.asCraftMirror(localItemStack);
+	}
 
 }
-
