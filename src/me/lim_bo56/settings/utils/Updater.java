69c4f3363a4ebc96ff9ac19c11213b9847bb4f5f
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2016-09-05 15:05:11 -0400

    Add files via upload

--- src/me/lim_bo56/settings/utils/Updater.java
+++ src/me/lim_bo56/settings/utils/Updater.java
@@ -35,7 +35,7 @@ public class Updater {
                     .replaceAll("[a-zA-Z ]", "");
             String OldVersion = plugin.getDescription().getVersion();
             if (!NewVersion.equals(OldVersion)) {
-                return "§dNew version available §b" + NewVersion + "\n" + "§aDownload§f>§7> §fhttp://bit.ly/lobbypref";
+                return "§dNew version available §b" + NewVersion + "\n" + "§aDownload§f>§7> §fhttp://bit.ly/PlayerSettings";
             }
         } catch (Exception ex) {
             plugin.getLogger().info("Failed to check for a update on spigot.");
