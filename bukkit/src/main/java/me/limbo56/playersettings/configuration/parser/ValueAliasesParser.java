4dff0b4983e3a9e167b89ce073c6d3218f08d41a
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2024-03-23 22:53:08 -0400

    Refactor value-aliases parser for clarity

--- bukkit/src/main/java/me/limbo56/playersettings/configuration/parser/ValueAliasesParser.java
+++ bukkit/src/main/java/me/limbo56/playersettings/configuration/parser/ValueAliasesParser.java
@@ -1,24 +1,25 @@
 package me.limbo56.playersettings.configuration.parser;
 
 import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.LinkedListMultimap;
-import com.google.common.collect.ListMultimap;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class ValueAliasesParser
     implements ConfigurationSectionParser<ImmutableListMultimap<String, Integer>> {
   public ImmutableListMultimap<String, Integer> parse(ConfigurationSection section) {
-    ListMultimap<String, Integer> valueAliases = LinkedListMultimap.create();
-    if (section != null) {
-      for (String key : section.getKeys(false)) {
-        int value = Integer.parseInt(key);
-        if (section.isList(key)) {
-          section.getStringList(key).forEach(alias -> valueAliases.put(alias, value));
-        } else if (section.isString(key)) {
-          valueAliases.put(section.getString(key), value);
-        }
+    if (section == null) {
+      return ImmutableListMultimap.of();
+    }
+
+    ImmutableListMultimap.Builder<String, Integer> builder = ImmutableListMultimap.builder();
+    for (String key : section.getKeys(false)) {
+      int value = Integer.parseInt(key);
+      if (section.isList(key)) {
+        section.getStringList(key).forEach(alias -> builder.put(alias, value));
+      } else if (section.isString(key)) {
+        builder.put(section.getString(key), value);
       }
     }
-    return ImmutableListMultimap.copyOf(valueAliases);
+
+    return builder.build();
   }
 }
